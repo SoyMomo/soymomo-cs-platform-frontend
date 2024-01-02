d490a3290cd9691a5aa90a6ce0a75a19072b6e1f
Nicolas <nicolasg@soymomo.com>
2024-01-02 16:49:36 -0300

    feat: se añaden funcionalidades para busqueda más holgada

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,13 @@
 
 import MainLayout from "../layouts/layout";
+import { ListItem, ListTitle } from "../components/ListItem";
 import { useNavigate } from   "react-router-dom";
-import { Input, message } from 'antd'
+import { Input, message, Button } from 'antd'
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../authContext";
 import axios from 'axios';
+import useQuery from "../utils/hooks/UseQuery";
+
 
 const { Search } = Input;
 
@@ -15,13 +18,48 @@ export default function Index() {
   const [messageApi, contextHolder] = message.useMessage();
   const key = 'updatable';
   const navigate = useNavigate();
+  const query = useQuery();
   const [inputValue, setInputValue] = useState('');
+  const [listItems, setListItems] = useState([]);
 
   useEffect(() => {
     if (!tokens) {
       navigate('/login');
     }
   }, [tokens, navigate]);
+
+  useEffect(() => {
+    const searchTxt = query.get('searchTxt');
+    if (searchTxt) {
+      onSearch(searchTxt)
+    }
+  }, [query])
+
+
+  useEffect(() => {
+    // When the component mounts, load the table state from sessionStorage
+    const savedTableState = sessionStorage.getItem('tableState');
+    if (savedTableState) {
+      setListItems(JSON.parse(savedTableState));
+    }
+  }, []);
+  
+  useEffect(() => {
+    // When the tableData state changes, save it to sessionStorage
+    if (listItems.length > 0) {
+      sessionStorage.setItem('tableState', JSON.stringify(listItems));
+    }
+  }, [listItems]);
+
+  const handleRowClick = (deviceId, imei) => {
+		const routeParam = deviceId ? `?deviceId=${deviceId}` : `?imei=${imei}`;
+        navigate(`/wearer${routeParam}`);
+	}
+
+  const cleanTable = () => {
+    setListItems([]);
+    sessionStorage.removeItem('tableState');
+  };
 
   async function onSearch(value) {
 
@@ -31,24 +69,12 @@ export default function Index() {
       content: 'Loading...',
     });
     
-    let params = {};
-
-    if (value.length === 10) {
-      params = { deviceId: value };
-    } else if (value.length === 15) {
-      params = { imei: value };
-    } else {
-      messageApi.open({
-        key,
-        type: 'error',
-        content: 'Invalid input, enter imei or deviceId!',
-        duration: 2,
-      });
-      return;
-    }
+    let params = {
+      queryStr: value
+    };
 
     try {
-      const response = await axios.get(process.env.REACT_APP_BACKEND_HOST + '/wearer/getWearerByDeviceIdOrImei', { 
+      const response = await axios.get(process.env.REACT_APP_BACKEND_HOST + '/wearer/getWearerByString', { 
         params: params, 
         headers: { 
           Authorization: `Bearer ${tokens.AccessToken}` 
@@ -61,7 +87,6 @@ export default function Index() {
           content: 'Not found!',
           duration: 2,
         });
-        setInputValue('');
       } else {
         messageApi.open({
           key,
@@ -69,8 +94,9 @@ export default function Index() {
           content: 'Loaded!',
           duration: 2,
         });
-        const routeParam = params.deviceId ? `?deviceId=${params.deviceId}` : `?imei=${params.imei}`;
-        navigate(`/wearer${routeParam}`);
+
+        setListItems(response.data.data)
+        console.log(listItems)
         
       }
     } catch(error) {
@@ -80,7 +106,6 @@ export default function Index() {
         content: 'Not found!',
         duration: 2,
       });
-      setInputValue('');
     }
   }
 
@@ -90,9 +115,44 @@ export default function Index() {
             <>
             {contextHolder}
             <div style={{ padding: 20 }}>
-              <div style={{ display: "flex", alignItems: "flex-start", marginBottom: 20 }}>
-                <Search placeholder="Buscar reloj por imei o deviceId" onSearch={onSearch} value={inputValue} onChange={(e) => setInputValue(e.target.value)} style={{ width: 500, padding: 5 }} />
+              <div style={{ display: "flex", alignItems: "center", marginBottom: 20 }}>
+                <Search
+                  placeholder="Buscar reloj por imei o deviceId"
+                  onSearch={onSearch}
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  style={{ width: 500, padding: 5 }}
+                />
+                {listItems.length > 0 && (
+                  <Button
+                    type="danger"
+                    onClick={cleanTable}
+                    style={{ marginLeft: '15px', backgroundColor: 'red' }}
+                  >
+                    Limpiar Tabla
+                  </Button>
+                )}
               </div>
+              {listItems.length !== 0 ? 
+                <div>
+                  <ListTitle/>
+                  <div className="list">
+                    {listItems.map((item, index) => 
+                      <ListItem
+                      objectId={item.objectId}
+                      deviceId={item.deviceId}
+                      firstName={item.firstName}
+                      lastName={item.lastName}
+                      imei={item.imei}
+                      phone={item.phone}
+                      handleClick={() => handleRowClick(item.deviceId, item.imei)}
+                      />
+                    )}
+                  </div>
+                </div> :
+                false
+              }
+              
             </div>
           </>
         }
