19705a6d3cd0faf9f10b45625968d0c39c7c5a84
Nicolas <nicolasg@soymomo.com>
2024-01-09 15:49:13 -0300

    feat: pequeña funcionalidad para cambiar contraseña. Falta lógica para funcionamiento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,19 @@
 import LoginForm from "../components/LoginForm"
 import styles from "../styles/ChangePasswordPage.module.css"
+// import { useNavigate } from "react-router-dom";
+// import { deleteCookie } from '../utils/cookies';
 
 
 export default function Login() {
+
+    // const navigate = useNavigate();
+
+    // const handlePasswordResetClick = () => {
+    //     deleteCookie('session');
+    //     navigate('/change-password')
+        
+    //     // window.location.href = '/change-password';
+    // }
     
     return (
         <div className={styles.generalContainer}>
@@ -17,7 +28,7 @@ export default function Login() {
 
 
                 <LoginForm/>
-                <a href="/change-password">
+                <a href='/change-password'>
                     <h3 className={styles.link}>¿Olvidaste tu contraseña?</h3>
                 </a>
                 <img src="images/cs-loginSoyMomoLogo.svg" width={100} height={100} className={styles.logo} alt="logo"/>
